0380dbe195224be79b2eda0ce5a7faa784e1a8ca
soomin <riato101@naver.com>
2023-02-16 18:03:54 +0900

    shared state

--- myproject/src/index.js
+++ myproject/src/index.js
@@ -11,10 +11,12 @@ import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
 import AttendaceBook from './chpater_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AttendaceBook />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 )
